f7d23d24c88ee1f6c78363eb07885ed228b6d145
佐藤亮介 <srrrs@satouryousukes-MacBook-Air.local>
2022-08-08 02:01:08 +0900

    rust

--- src/modules/mod_test.rs
+++ src/modules/mod_test.rs
@@ -2,11 +2,28 @@
 
 #[cfg(test)]
 mod tests {
+
     #[test]
     fn works() {
         assert_eq!(add_two(2), 4);
     }
+
     fn add_two(n: i32) -> i32 {
         n + 2
+    }
+
+    #[test]
+    #[should_panic(expected = "panic error function")]
+    fn error_func() {
+        panic!("panic error function");
+    }
+
+    #[test]
+    fn if_test() -> Result<(), String> {
+        if 2 * 2 == 4 {
+            Ok(())
+        } else {
+            Err(String::from("error function"))
+        }
     }
 }
